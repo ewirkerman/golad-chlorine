949b83d1587e31e0c2f469fd75168eb01300d7f8
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2018-01-30 18:35:59 +0000

    cleanup

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const ipcMain = require("electron").ipcMain;
 const path = require("path");
 const windows = require("./modules/windows");
 
-let about_message = `Chlorine: Replay viewer for Halite 2\n` +
+let about_message = `Chlorine-Golad: Replay viewer for Golad\n` +
 					`--\n` +
 					`Electron ${process.versions.electron}\n` +
 					`Node ${process.versions.node}\n` +
@@ -19,17 +19,6 @@ electron.app.on("ready", () => {
 	windows.new("renderer", {
 		title: "Chlorine", show: true, width: 1200, height: 800, resizable: true, page: path.join(__dirname, "chlorine_renderer.html")
 	});
-
-	
-	/*
-	windows.new("info", {
-		title: "Info", show: false, width: 400, height: 800, resizable: true, page: path.join(__dirname, "chlorine_info.html")
-	});
-
-	windows.new("events", {
-		title: "Events", show: false, width: 400, height: 800, resizable: true, page: path.join(__dirname, "chlorine_info.html")
-	});
-	*/
 
 	electron.Menu.setApplicationMenu(make_main_menu());
 });
@@ -99,37 +88,6 @@ function make_main_menu() {
 					type: "separator"
 				},
 				{
-					label: "Save decompressed JSON...",
-					accelerator: "CommandOrControl+S",
-					click: () => {
-						let outfilename = electron.dialog.showSaveDialog();
-						if (outfilename) {
-							windows.send("renderer", "save", outfilename);
-						}
-					}
-				},
-				{
-					label: "Save current entities...",
-					click: () => {
-						let outfilename = electron.dialog.showSaveDialog();
-						if (outfilename) {
-							windows.send("renderer", "save_frame", outfilename);
-						}
-					}
-				},
-				{
-					label: "Save current moves...",
-					click: () => {
-						let outfilename = electron.dialog.showSaveDialog();
-						if (outfilename) {
-							windows.send("renderer", "save_moves", outfilename);
-						}
-					}
-				},
-				{
-					type: "separator"
-				},
-				{
 					accelerator: "CommandOrControl+Q",
 					role: "quit"
 				},
@@ -181,154 +139,6 @@ function make_main_menu() {
 				},
 			]
 		},
-		/*{
-			label: "View",
-			submenu: [
-				{
-					label: "Reset Zoom / Scroll",
-					accelerator: "F1",
-					click: () => {
-						windows.send("renderer", "reset_zoom", null);
-					}
-				},
-				{
-					type: "separator"
-				},
-				{
-					label: "Weapon Ranges",
-					click: () => {
-						windows.send("renderer", "toggle", "weapon_ranges");
-					},
-					type: "checkbox",
-					checked: false,
-				},
-				{
-					label: "Tactical Ranges (13)",
-					submenu: [
-						{
-							label: "Pink",
-							accelerator: "1",
-							click: () => {
-								windows.send("renderer", "toggle", "tactical_ranges_0");
-							},
-							type: "checkbox",
-							checked: false,
-						},
-						{
-							label: "Blue",
-							accelerator: "2",
-							click: () => {
-								windows.send("renderer", "toggle", "tactical_ranges_1");
-							},
-							type: "checkbox",
-							checked: false,
-						},
-						{
-							label: "Yellow",
-							accelerator: "3",
-							click: () => {
-								windows.send("renderer", "toggle", "tactical_ranges_2");
-							},
-							type: "checkbox",
-							checked: false,
-						},
-						{
-							label: "Green",
-							accelerator: "4",
-							click: () => {
-								windows.send("renderer", "toggle", "tactical_ranges_3");
-							},
-							type: "checkbox",
-							checked: false,
-						},
-					]
-				},
-				{
-					label: "Docking Ranges",
-					click: () => {
-						windows.send("renderer", "toggle", "docking_ranges");
-					},
-					type: "checkbox",
-					checked: false,
-				},
-				{
-					type: "separator"
-				},
-				{
-					label: "Planets",
-					click: () => {
-						windows.send("renderer", "toggle", "show_planets");
-					},
-					type: "checkbox",
-					checked: true,
-				},
-				{
-					type: "separator"
-				},
-				{
-					label: "Tails",
-					accelerator: "T",
-					click: () => {
-						windows.send("renderer", "toggle", "tails");
-					},
-					type: "checkbox",
-					checked: true,
-				},
-				{
-					label: "Pointers",
-					accelerator: "P",
-					click: () => {
-						windows.send("renderer", "toggle", "pointers");
-					},
-					type: "checkbox",
-					checked: false,
-				},
-				{
-					label: "Deaths",
-					click: () => {
-						windows.send("renderer", "toggle", "deaths");
-					},
-					type: "checkbox",
-					checked: true,
-				},
-				{
-					type: "separator"
-				},
-				{
-					label: "Selection Crosshair",
-					click: () => {
-						windows.send("renderer", "toggle", "crosshair");
-					},
-					type: "checkbox",
-					checked: true,
-				},
-				{
-					label: "High Precision Coordinates",
-					click: () => {
-						windows.send("renderer", "toggle", "precise");
-					},
-					type: "checkbox",
-					checked: false,
-				},
-				{
-					type: "separator"
-				},
-				{
-					label: "Angle Message Interpretation",		// Consider angles >= 360 as encoding a number...
-					click: () => {
-						windows.send("renderer", "toggle", "angle_messages");
-					},
-					type: "checkbox",
-					checked: true,
-				},
-				{
-					label: "About Angle Messages...",
-					click: () => {
-						about_angle_messages();
-					}
-				},
-			]
-		},*/
 		{
 			label: "Windows",
 			submenu: [
@@ -355,31 +165,4 @@ function make_main_menu() {
 	];
 
 	return electron.Menu.buildFromTemplate(template);
-}
-
-function about_angle_messages() {
-
-	let s = `
-Want your ships to write messages directly into the game replay? \
-Of course you do! Introducing Chlorine's patented Angle Message \
-system! Exploit the redundancy inherent in angles to send messages \
-to yourself!
-
-How it works:
-
-A Halite angle is an unsigned int16. There are just over 180 ways \
-of sending the same angle. Chlorine can interpret ship movements \
-with angles over 359 as messages, where each message is a number \
-in the range 0..180.
-
-To encode:
-
-    def new_angle_with_message(angle, message):
-        return ((message + 1) * 360) + angle
-
-Then use the new angle when sending the ship's thrust to the server. \
-Chlorine can show the message in the ship info. You can also edit \
-messages.json to display text rather than numbers.`;
-
-	alert(s);
 }
